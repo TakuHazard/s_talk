da6c6f6a4866d555728c8a55f6cdfd1aa276b2db
tmarwend <taku_marwendo@sfu.ca>
2020-10-08 21:14:53 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "list.h"
+
+int data [300];
+
+int main(){
+	List* newList = List_create();
+
+	for(int i = 0; i < 300; i++){
+		data[i] = i;
+	}
+
+	for(int i = 0; i < 50; i++){
+		List_append(newList, &data[i]);
+	}
+	printf("Hello world !\n");
+
+}
