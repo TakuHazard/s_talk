5481a515f0dacdb30b539f1495a50e5d439ef1b2
tmarwend <taku_marwendo@sfu.ca>
2020-10-11 00:08:04 -0700

    Added addressinfo capability

--- network.c
+++ network.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
+// #include <stdio.h>
 #include <stdlib.h>
-#include <netdb.h>
-#include <string.h>
+// #include <netdb.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 #define MSG_MAX_LEN 1024
 #define PORT 22110
@@ -74,14 +80,49 @@ int main(int argc, char* argv[]){
 
 	int localPortNum = atoi(givenPortNum);
 	int remotePortNum = atoi(givenRemotePortNum);
-
 	strcat(givenHostName,".cs.surrey.sfu.ca");
-	
 	printf("This are the commandline args %d %s %d \n", localPortNum, givenHostName, remotePortNum);
 
 	printf("This is the prot %d\n", PORT);
 	printf("Connect using : \n");
 	printf("       netcat -u 127.0.0.1 %d\n", PORT);
+
+	struct addrinfo hints, *res, *p;
+    int status;
+    char ipstr[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo(givenHostName, NULL, &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
+    }
+
+    for(p = res;p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
+
+        // get the pointer to the address itself,
+        // different fields in IPv4 and IPv6:
+        if (p->ai_family == AF_INET) { // IPv4
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else { // IPv6
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        // convert the IP to a string and print it:
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("  %s: %s\n", ipver, ipstr);
+    }
+
+    freeaddrinfo(res); // free the linked list
+
 
 
 	struct sockaddr_in sin;
@@ -115,4 +156,6 @@ int main(int argc, char* argv[]){
 	return 0;
 
 }
+
+
 
