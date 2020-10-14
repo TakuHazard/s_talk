5be588991be1dc1339bb1820fde3941876280c11
ndoctor <ndoctor@asb9700u-d03.csil.sfu.ca>
2020-10-13 21:55:12 -0700

    Cleaned up network.c, removed bytesRx

--- network.c
+++ network.c
@@ -1,6 +1,4 @@
-// #include <stdio.h>
 #include <stdlib.h>
-// #include <netdb.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
@@ -26,7 +24,6 @@
 
 static struct sockaddr_in sinRemote;
 static int socketDescriptor;
-static int bytesRx;
 static unsigned int sin_len;
 
 
@@ -42,27 +39,12 @@ void *get_in_addr(struct sockaddr *sa)
 void* sendThread(void* unused){
 	bool continueSending = true;
 	while(continueSending){
-		// char messageToSend[MSG_MAX_LEN];
-		// printf("Here in sendThreadRx Type message to send\n");
-		// fgets(messageToSend,sizeof messageToSend, stdin);
-		// printf("Here in sendThreadRx messageTosend is %s", messageToSend);
-
-		// int terminateIdx = (bytesRx < MSG_MAX_LEN) ? bytesRx : MSG_MAX_LEN;
-		// messageRx[terminateIdx] = 0;
-
-		// //Extract the value from the message :
-		// // (process the message any way your app requires)
-		// int incMe = atoi(messageRx);
 
 		// Compose the reply message
 		char messageTx[MSG_MAX_LEN];
-		// printf("Inside threadSendPID\n");
 		fgets(messageTx, MSG_MAX_LEN, stdin);
-		// sprintf(messageTx, "this is our reply\n");
-
 
 		// Transmit a reply
-
 		sin_len = sizeof(sinRemote);
 		sendto(socketDescriptor, messageTx, strlen(messageTx), 0, (struct sockaddr*) &sinRemote, sin_len);
 
@@ -76,12 +58,12 @@ void* sendThread(void* unused){
 }
 
 void* receiveThread(void* unused){
-	//Address
+	// Address
 	while(1){
 		sin_len = sizeof(sinRemote);
 		char messageRx[MSG_MAX_LEN];
 		int bytesRx = recvfrom(socketDescriptor, messageRx, MSG_MAX_LEN, 0, (struct sockaddr*) &sinRemote, &sin_len);
-		// printf("RECEIVED SOMETHING %d\n", bytesRx);
+		
 		int terminateIdx = (bytesRx < MSG_MAX_LEN) ? bytesRx : MSG_MAX_LEN;
 		messageRx[terminateIdx] = 0;
 		printf("%s", messageRx);
@@ -100,49 +82,6 @@ int main(int argc, char* argv[]){
 	int remotePortNum = atoi(givenRemotePortNum);
 
 	printf("PORTNUM %d hostname %s remotePortNum %d\n", localPortNum, givenHostName, remotePortNum);
-
-
-	// GET ADDRESSINFO FOR THE OTHER PEER
-	// struct addrinfo hints, *res;
-	
-	// memset(&hints, 0, sizeof hints);
-	// hints.ai_family = AF_UNSPEC;
-	// hints.ai_socktype = SOCK_DGRAM;
-
-	// if((rv = getaddrinfo(givenHostName, givenRemotePortNum, &hints, &servinfo)) != 0){
-	// 	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-	// 	return 1;
-	// }
-
-	// for(p = servinfo; p != NULL; p = p->ai_next) {
- //        if ((socketDescriptor = socket(p->ai_family, p->ai_socktype,
- //                p->ai_protocol)) == -1) {
- //            perror("client: socket");
- //            continue;
- //        }
-
- //        if (connect(socketDescriptor, p->ai_addr, p->ai_addrlen) == -1) {
- //            close(socketDescriptor);
- //            perror("client: connect");
- //            continue;
- //        }
-
- //        break;
- //    }
-
- //    if (p == NULL) {
- //        fprintf(stderr, "client: failed to connect\n");
- //        return 2;
- //    }
-
- //    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
- //            s, sizeof s);
- //    printf("client: connecting to %s\n", s);
- //    freeaddrinfo(servinfo);
-	
-	// getaddrinfo(givenHostName,givenPortNum,&hints,&res);
-
-
 
 	struct addrinfo hints, *res,*p;
 	memset(&hints, 0, sizeof hints);
@@ -194,7 +133,6 @@ int main(int argc, char* argv[]){
 
 	memset(&sinRemote, 0, sizeof(sinRemote));
 	sinRemote.sin_family = res->ai_family;
-	// sinRemote.sin_addr.s_addr = htonl(INADDR_ANY);
 	sinRemote.sin_port = htons(remotePortNum);
 	inet_pton(AF_INET,ipstr,&(sinRemote.sin_addr));
 
