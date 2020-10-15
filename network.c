7537cdc93ac634130e206afa797587e5ddae7979
tmarwend <taku_marwendo@sfu.ca>
2020-10-14 17:46:23 -0700

    More refactoring

--- network.c
+++ network.c
@@ -9,15 +9,7 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-
 #include <errno.h>
-#include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-
-#include <arpa/inet.h>
 
 // #include "keyboardProducer.h"
 // #include "screenConsumer.h"
@@ -32,48 +24,6 @@ struct socketStuff{
 	int socketDescriptor;
 	struct sockaddr_in sin;
 };
-
-/*char* createAddrInfo(char* givenHostName) {
-	struct addrinfo hints, *res,*p;
-	
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_DGRAM;
-
-	int rv = getaddrinfo(givenHostName, NULL, &hints, &res);
-
-	if(rv < 0) {
-		printf("ERROR \n");
-		return NULL;
-	}
-
-	char* ipstr = malloc(sizeof(char)*INET6_ADDRSTRLEN);
-
-	for(p = res;p != NULL; p = p->ai_next) {
-        void *addr;
-        char *ipver;
-
-        // get the pointer to the address itself,
-        // different fields in IPv4 and IPv6:
-        if (p->ai_family == AF_INET) { // IPv4
-            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-            addr = &(ipv4->sin_addr);
-            ipver = "IPv4";
-        } else { // IPv6
-            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-            addr = &(ipv6->sin6_addr);
-            ipver = "IPv6";
-        }
-
-        // convert the IP to a string and print it:
-        inet_ntop(p->ai_family, addr, &ipstr[0], sizeof ipstr);
-        printf("  %s: %p\n", ipver, ipstr);
-    }
-
-    freeaddrinfo(res); // free the linked list
-
-	return ipstr;
-}*/
 
 void* sendThread(void* socketInput){
 	struct socketStuff* socketInfo = (struct socketStuff* ) socketInput;
@@ -116,20 +66,10 @@ void* receiveThread(void* socketInput){
 	return NULL;
 }
 
-int main(int argc, char* argv[]){
-	int socketDescriptor;
-	struct sockaddr_in sinRemote;
-	char* givenPortNum = argv[1];
-	char* givenHostName = argv[2];
-	char* givenRemotePortNum = argv[3];
 
-	int localPortNum = atoi(givenPortNum);
-	int remotePortNum = atoi(givenRemotePortNum);
-
-
-	printf("PORTNUM %d hostname %s remotePortNum %d\n", localPortNum, givenHostName, remotePortNum);
-
-	//char* ipstr = createAddrInfo(givenHostName);
+int createSockets(int localPortNum, int remotePortNum, char* givenHostName, struct sockaddr_in* sinLocalInput, struct sockaddr_in* sinRemoteInput){
+	// struct sockaddr_in sin = *sinLocalInput;
+	// struct sockaddr_in sinRemote = *sinRemoteInput;
 
 	struct addrinfo hints, *res,*p;
 	memset(&hints, 0, sizeof hints);
@@ -142,10 +82,6 @@ int main(int argc, char* argv[]){
 		printf("ERROR \n");
 		return 1;
 	}
-
-	// printf("Checking results after getaddrinfo \n");
-	// printf("ai_flags %d ai_family %d ai_socktype %d ai_cannonname %s \n",
-	// 	res->ai_flags, res->ai_family, res->ai_socktype, res->ai_canonname);
 
 	char ipstr[INET6_ADDRSTRLEN];
 
@@ -172,26 +108,49 @@ int main(int argc, char* argv[]){
 
     freeaddrinfo(res); // free the linked list
 	
+	memset(sinLocalInput, 0, sizeof(*sinLocalInput));
+	(*sinLocalInput).sin_family = AF_INET;
+	(*sinLocalInput).sin_addr.s_addr = htonl(INADDR_ANY);
+	(*sinLocalInput).sin_port = htons(localPortNum);
+
+	memset(sinRemoteInput, 0, sizeof(*sinRemoteInput));
+	(*sinRemoteInput).sin_family = AF_INET;
+	(*sinRemoteInput).sin_port = htons(remotePortNum);
+	inet_pton(AF_INET,ipstr,&(sinRemoteInput->sin_addr));
+
+
+	return 0;
+}
+
+int main(int argc, char* argv[]){
+	int socketDescriptor;
+	char* givenPortNum = argv[1];
+	char* givenHostName = argv[2];
+	char* givenRemotePortNum = argv[3];
+
+	int localPortNum = atoi(givenPortNum);
+	int remotePortNum = atoi(givenRemotePortNum);
+
+
+	printf("PORTNUM %d hostname %s remotePortNum %d\n", localPortNum, givenHostName, remotePortNum);
+
+	//char* ipstr = createAddrInfo(givenHostName);
+
 	struct sockaddr_in sin;
-	memset(&sin, 0, sizeof(sin));
-	sin.sin_family = AF_INET;
-	sin.sin_addr.s_addr = htonl(INADDR_ANY);
-	sin.sin_port = htons(localPortNum);
-
-
-	memset(&sinRemote, 0, sizeof(sinRemote));
-	sinRemote.sin_family = AF_INET;
-	sinRemote.sin_port = htons(remotePortNum);
-	inet_pton(AF_INET,ipstr,&(sinRemote.sin_addr));
-
-
+	struct sockaddr_in sinRemote;
+	int socketResult = createSockets(localPortNum, remotePortNum, givenHostName, &sin, &sinRemote);
+	if(socketResult != 0){
+		printf("Connection failed\n");
+		return 1;
+	}
 	// Create the socket for UDP
 	socketDescriptor = socket(AF_INET,SOCK_DGRAM,0);
 
+
+	// printf("After returning we have the following local %d %s  remote %d %s\n", sin.sin_port, sin.sin_addr, sinRemote.sin_port, sinRemote.sin_addr);
 	// Bind the socket to the port (PORT) that we specify
 	bind(socketDescriptor, (struct sockaddr*) &sin, sizeof(sin));
-
-
+	
 	struct socketStuff sockInfo;
 	sockInfo.socketDescriptor = socketDescriptor;
 	sockInfo.sin = sinRemote;
