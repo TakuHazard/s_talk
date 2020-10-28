7f9a847c8f69c8eace411b2da8047aa4fef686cb
tmarwend <taku_marwendo@sfu.ca>
2020-10-27 22:40:41 -0700

    Removed f-gets

--- screenConsumer.c
+++ screenConsumer.c
@@ -58,8 +58,10 @@ void* printThread() {
             pthread_mutex_unlock(&mutexShutdown);
 
         } else {
-            fputs(msg, stdout);
-            List_free(remoteList, *messageDestructorCons);
+            // fputs(msg, stdout);
+            write(stdout,msg,MSG_MAX_LEN);
+            // List_free(remoteList, *messageDestructorCons);
+            free(msg);
             msg = NULL;
         }
 	}
